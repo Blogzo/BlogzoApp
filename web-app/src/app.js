b67333818eebc7c2572802b79a87f4fd24e85090
keriki101 <kireerik2008@hotmail.com>
2020-01-27 11:37:01 +0100

    fixed first image for app

--- web-app/src/app.js
+++ web-app/src/app.js
@@ -3,11 +3,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(request, response){
-    
-    response.send("Hello, world!")
+  response.send("Hello, World")
 })
 
 app.listen(8080, function(){
-
-    console.log("Web app listening on port 8080.")
+  console.log("Web application listening on port 8080.")
 })
